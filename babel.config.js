edc957e99b2c7beedc338c939e3dfe5074b6cb15
Emmanuel Ezekiel <emamex44@gmail.com>
2023-11-24 12:54:28 +0100

    folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@redux': './src/redux',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
